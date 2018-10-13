8d10148775fe35f4d421efccf5a5a34f768473fb
Nathan Salazar <nathan.salazar20@gmail.com>
2018-10-13 15:21:08 -0500

    set up POST route to database

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../modules/pool');
 
 router.post('/',(req,res)=>{
     console.log('req.body:',req.body);
+    pool.query(`INSERT INTO "feedback" 
+        ("feeling", "understanding", "support", "comments")
+        VALUES ($1,$2,$3,$4);`,
+        [req.body.feeling, req.body.understanding, req.body.supported, 
+            req.body.comments]).then((results) => {
+                console.log('results',results);
+            }).catch((error)=>{
+                console.log('Error in POST:',error);
+            })
 })
 
 module.exports = router;
